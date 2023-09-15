2b40f86a9a0e2e4fe436abd4f48ae55aa38b631e
paras-017 <paraschauhan0017@gmail.com>
2023-09-15 16:08:39 +0530

    basics 02_day

---
+++ 02_PreReq_Install/main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// sudo npm install -g typescript(for linux)
+// npm install -g typescript(for window)
+var num = 912;
+// num = "paras"     //GivesError: Type 'string' is not assignable to type 'number'
+num = 5532;
---
+++ 02_PreReq_Install/main.ts
@@ -0,0 +1,10 @@
+// to install typescript: sudo npm install -g typescript(for linux)
+// npm install -g typescript(for window)
+
+
+let num:number = 912           
+
+// num = "paras"     //GivesError: Type 'string' is not assignable to type 'number'
+num = 5532
+
+export {};
