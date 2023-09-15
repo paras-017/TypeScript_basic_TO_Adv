a37d8886a7498edc44c9a2a6afa6646f3a79f853
paras-017 <paraschauhan0017@gmail.com>
2023-09-15 21:32:35 +0530

    Arrow types added

--- 07_Functions/main.js
+++ 07_Functions/main.js
@@ -1,4 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 function greet(name, id) {
-    console.log("My name is ".concat(name, " and id is ").concat(id));
+    return "My name is ".concat(name, " and id is ").concat(id);
 }
-greet('Paras', 389240238);
+console.log(greet('Paras', 389240238));
+var greet2 = function (name, id) {
+    return "My name is ".concat(name, " and id is ").concat(id);
+};
+console.log(greet2('Paras', 389240238));
--- 07_Functions/main.ts
+++ 07_Functions/main.ts
@@ -1,4 +1,10 @@
 function greet(name:string, id:number):string{
   return `My name is ${name} and id is ${id}`
 }
-greet('Paras',389240238)
+console.log(greet('Paras',389240238))
+
+const greet2 =(name:string, id:number):string =>{
+  return `My name is ${name} and id is ${id}`
+}
+console.log(greet2('Paras',389240238))
+export{}
