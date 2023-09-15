356c0561911c98e79b9f0d4f6d383b17ebe4c667
paras-017 <paraschauhan0017@gmail.com>
2023-09-15 20:58:12 +0530

    type checked of promise

--- 06_Any_Unknown/main2.ts
+++ 06_Any_Unknown/main2.ts
@@ -5,6 +5,5 @@ variable = true
 if(typeof variable === 'number'){                          //Typescript doesn't treat these string literals as actual strings but as type indicators, so "number" is not string its actually number type in typescript
     console.log(variable+8)
 }else if(typeof variable === 'boolean'){
-    
     console.log(variable)
 }
