26d99f5af3e6e4f555077805bc40ce8f75cbbc2b
paras-017 <paraschauhan0017@gmail.com>
2023-09-15 21:43:08 +0530

    Type inference

---
+++ 08_typeInference/main.ts
@@ -0,0 +1,2 @@
+// Type Inference in TS refers to the ability of the TS compiler to automatically determine and assign type to variables, expressions, and function return values based on their usage and context in the code.
+const myName = 'Paras'
