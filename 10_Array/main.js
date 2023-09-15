cde06b16fe4060b0937aca48c50dc3931021fb40
paras-017 <paraschauhan0017@gmail.com>
2023-09-15 22:07:07 +0530

    Array in Typescript

---
+++ 10_Array/main.js
@@ -0,0 +1,8 @@
+// Defining Array
+// 1st way  Square Brackets
+var numbers = [1, 2, 2, 3, 3, 4, 5];
+// 2nd Array constructor
+var numbers1 = new Array(2, 32, 312, 31, 25, 4);
+// 3rd Array.of method
+var names = Array.of("Paras", "Singh", "Chauhan");
+console.log(names[1]);
---
+++ 10_Array/main.ts
@@ -0,0 +1,11 @@
+// Defining Array
+
+// 1st way  Square Brackets
+const numbers:number[] = [1,2,2,3,3,4,5]
+
+// 2nd Array constructor
+const numbers1:number[] = new Array(2,32,312,31,25,4)
+
+// 3rd Array.of method
+const names:string[] = Array.of("Paras","Singh", "Chauhan")
+console.log(names[1])
