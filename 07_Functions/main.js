4c5da627e126a099df001645fc394373e7179318
paras-017 <paraschauhan0017@gmail.com>
2023-09-15 21:28:35 +0530

    normal function

---
+++ 07_Functions/main.js
@@ -0,0 +1,4 @@
+function greet(name, id) {
+    console.log("My name is ".concat(name, " and id is ").concat(id));
+}
+greet('Paras', 389240238);
---
+++ 07_Functions/main.ts
@@ -0,0 +1,4 @@
+function greet(name:string, id:number):string{
+  return `My name is ${name} and id is ${id}`
+}
+greet('Paras',389240238)
