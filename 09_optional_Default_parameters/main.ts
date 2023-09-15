f58e21a803ef176dbc8e2572dfd50e6aa75d0080
paras-017 <paraschauhan0017@gmail.com>
2023-09-15 21:56:18 +0530

    comments added inn 08

--- 09_optional_Default_parameters/main.ts
+++ 09_optional_Default_parameters/main.ts
@@ -1,10 +1,10 @@
-
+//  Default parameter
 const greet1 = (name:string, id:number=31):string=>{
   return `Welcome ${name} and your id is ${id}`
 }
 console.log(greet1("paras",34523))
 
-
+//  Optional parameter:they used by appending a "?" symbol after the parameter name
 const greet2 = (name:string, id:number=31, city?:string):string=>{
   if(city) return `Welcome ${name} and your id is ${id}, ${city}`
   return `Welcome ${name} and your id is ${id}`
