b19120be75939dd3c81d634dba42cd7a61c209b9
paras-017 <paraschauhan0017@gmail.com>
2023-09-16 18:03:40 +0530

    Type aliase in TS

---
+++ 14_TypesAliases/main.ts
@@ -0,0 +1,19 @@
+// In TS, a type alias is a way to give a name to specific type or combination of types. It follows you to create a custom name for a type, making it easier to reuse and refer to the same type in different parts of your code. Type aliases provide better readabiltiy , oraganisation and abstraction of complex code
+
+type Person= {
+  name:string;
+  id:number;
+  isFresher:boolean;
+  package:number;
+  city:string;
+  experience?:number; //optional Property
+  stack:string[]
+}
+const fresher:Person={
+    name:"Paras Singh",
+    id:283213,
+    isFresher:true,
+    package:48000000,
+    city:"Muzaffarnagar",
+    stack:["Javascript", "Typescript", "Python", "Full Stack Developer"]
+}
