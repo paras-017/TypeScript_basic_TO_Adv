d980bac2402fb22e470685a1d633447618d51186
paras-017 <paraschauhan0017@gmail.com>
2023-09-16 21:47:07 +0530

    .

--- 17_tuples/main.ts
+++ 17_tuples/main.ts
@@ -9,6 +9,7 @@ const person1:Person = ['Paras','Singh', 18, true]
 const displayPersonInfo:(person:Person)=>string = (person:Person)=>{
     // return `${person[0]} ${person[1]} you age ${person[2]} and software show your driving licence is ${person[3]}`
     // OR you can do like this
+    
     const [fname, lname,age, isLicence] = person
     return `${fname} ${lname} you age is ${age} and driving license is ${isLicence?`availble for you`:`not available yer`}`
 }
