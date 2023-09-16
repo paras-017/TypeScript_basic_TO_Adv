5d9943b22564530fb103a13f9e6bc7aeb14ebf7e
paras-017 <paraschauhan0017@gmail.com>
2023-09-16 07:31:48 +0530

    for...in and for...of

--- 11_ArrayMethods/main.js
+++ 11_ArrayMethods/main.js
@@ -1,3 +1,18 @@
 var fruits = ['apple', 'banana', 'orange'];
-var newUpdatedFruits = fruits.push('Banana');
+var newUpdatedFruits = fruits.push('Banana'); //push method returns a length of an new array
 console.log(newUpdatedFruits);
+var removeLastElement = fruits.pop(); //remove and return the last element
+console.log(removeLastElement);
+// Loops    1st for
+for (var i = 0; i < fruits.length; i++) {
+    console.log(i);
+}
+// Loops    2nd for..in   (gives index)
+for (var i in fruits) {
+    console.log(i);
+}
+// Loops    2nd for..of   (gives value)
+for (var _i = 0, fruits_1 = fruits; _i < fruits_1.length; _i++) {
+    var i = fruits_1[_i];
+    console.log(i);
+}
--- 11_ArrayMethods/main.ts
+++ 11_ArrayMethods/main.ts
@@ -1,3 +1,21 @@
 const fruits:string[] = ['apple','banana', 'orange']
 const newUpdatedFruits = fruits.push('Banana')          //push method returns a length of an new array
 console.log(newUpdatedFruits)
+
+const removeLastElement = fruits.pop()                  //remove and return the last element
+console.log(removeLastElement)
+
+// Loops    1st for
+for (let i = 0; i < fruits.length; i++) {
+    console.log(i) 
+}
+
+// Loops    2nd for..in   (gives index)
+for (const i in fruits) {
+    console.log(i)
+}
+
+// Loops    2nd for..of   (gives value)
+for (const i of fruits) {
+    console.log(i)
+}
