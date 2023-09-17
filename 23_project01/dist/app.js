a5bdeac9562e8fa547d9f2481731b0b9f7c26e37
paras-017 <paraschauhan0017@gmail.com>
2023-09-17 18:50:37 +0530

    TS, wactchMOde, RootDIR, Lib,exclude, include

---
+++ 23_project01/dist/app.js
@@ -0,0 +1,6 @@
+"use strict";
+const fresher1 = {
+    name: 'Aakash',
+    age: 21,
+    isFresher: true
+};
---
+++ 23_project01/src/app.ts
@@ -0,0 +1,11 @@
+type Person = {
+    name: string,
+    age: number,
+    isFresher:boolean
+}
+
+const fresher1:Person = {
+    name:'Aakash',
+    age:21,
+    isFresher:true
+}
