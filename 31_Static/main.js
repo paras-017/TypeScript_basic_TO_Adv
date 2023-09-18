311396692fcba2619870acdf3022e92757e47660
paras-017 <paraschauhan0017@gmail.com>
2023-09-18 17:40:53 +0530

    static

---
+++ 31_Static/main.js
@@ -0,0 +1,7 @@
+var MathOperations = /** @class */ (function () {
+    function MathOperations() {
+    }
+    MathOperations.PI = Math.PI;
+    return MathOperations;
+}());
+console.log(MathOperations.PI * 1000);
---
+++ 31_Static/main.ts
@@ -0,0 +1,4 @@
+class MathOperations{
+    public static PI:number = Math.PI
+}
+console.log(MathOperations.PI*1000)
