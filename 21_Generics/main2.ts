679b4e3cce9d146da3bf7042afa96b67ccf5bdfd
paras-017 <paraschauhan0017@gmail.com>
2023-09-24 18:42:13 +0530

    generics in class

--- 21_Generics/main2.ts
+++ 21_Generics/main2.ts
@@ -11,3 +11,31 @@ function swap<T>(a: T, b: T): [T, T] {
 }
 
 const swapped = swap(10, 20); // swapped is of type [number, number]
+
+// In this example, T is a type parameter that represents the type of the arguments passed to the swap function. TypeScript infers the types when you call the function.
+
+// Creating a Generic Class:
+
+// You can create generic classes by specifying type parameters in the class definition. Here's an example of a generic Stack class:
+
+class Stack<T> {
+    private items: T[] = [];
+
+    push(item: T): void {
+        this.items.push(item);
+    }
+
+    pop(): T | undefined {
+        return this.items.pop();
+    }
+}
+
+const numberStack = new Stack<number>();
+numberStack.push(1);
+numberStack.push(2);
+const poppedNumber = numberStack.pop(); // poppedNumber is of type number
+
+const stringStack = new Stack<string>();
+stringStack.push("hello");
+stringStack.push("world");
+const poppedString = stringStack.pop(); // poppedString is of type string
